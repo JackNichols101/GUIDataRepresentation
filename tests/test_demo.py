40a0bd6cbfca807d6c4a4f03b05d765f9d5e1bb1
GasDuck <jnrnichols@gmail.com>
2021-02-28 16:17:07 -0500

    fixing test 2

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -10,7 +10,7 @@ def test_fill_db():
     connection, cursor = Demo.open_db('test_data.db')
     Demo.setup_db(cursor)
     test_list = [{'2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line': 2,
-                  '2017.student.size': 4,
+                  '2017.student.size': 4, 'school.name': 'test_name',
                   '2018.student.size': 3, 'school.state': 'test_state', 'id': 1,
                   'school.city': 'test_city', '2016.repayment.3_yr_repayment.overall': 5}]
     Demo.fill_db(test_list, cursor)
